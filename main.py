bcdb8a330be2e8d32ba3641ae4c94cd72db4d7b2
Aleksandr <allex220@gmail.com>
2024-09-14 20:08:51 +0500

    ДОбавлены миграции

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from sqlalchemy import create_engine
-
-
-
-# Констринг для подключения к базе данных
-DATABASE_URL = "postgresql+psycopg2://postgres:postgresql@localhost/sprint"
-
-# Создание объекта Engine
-engine = create_engine(DATABASE_URL, echo=True)
-
-#подключаемся к базе
-engine.connect()
+# from sqlalchemy import create_engine
+#
+#
+#
+# # Констринг для подключения к базе данных
+# DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost/sprint"
+#
+# # Создание объекта Engine
+# engine = create_engine(DATABASE_URL, echo=True)
+#
+# #подключаемся к базе
+# engine.connect()
+# print(engine)
 
 
 
--- models_2.py
+++ models_2.py
@@ -1,39 +1,47 @@
-from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 
 Base = declarative_base()
 
-DATABASE_URL = "postgresql+psycopg2://postgres:postgresql@localhost/sprint"
+DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost/sprint"
 
 engine = create_engine(DATABASE_URL, echo=True)
+
 
 class SStatus(Base):
     __tablename__ = 'status_users'
     status_id = Column(Integer, primary_key=True, autoincrement=True)
     status_name = Column(String)
-    discont = Column(String)
+    discount = Column(String)
+
 
 class SUsers(Base):
     __tablename__ = 'users'
     user_id = Column(Integer, primary_key=True, autoincrement=True)
-    user_name = Column(String,nullable=False)
+    user_name = Column(String, nullable=False)
     password = Column(String, nullable=False)
-    email = Column(String,nullable=False)
-    telephone = Column(String,nullable=False)
+    email = Column(String, nullable=False)
+    telephone = Column(String, nullable=False)
     status = Column(ForeignKey('status_users.status_id'))
-    date_created = Column(DateTime, default=datetime.datetime.utcnow)
+    date_created = Column(DateTime)
     favorit_product = Column(String)
+
 
 class SShops(Base):
     __tablename__ = 'shops'
     shop_id = Column(Integer, primary_key=True, autoincrement=True)
     shop_name = Column(String)
 
+
 class Scategories(Base):
     __tablename__ = 'categories'
     category_id = Column(Integer, primary_key=True, autoincrement=True)
     category_name = Column(String)
+
 
 class SProducts(Base):
     __tablename__ = 'products'
@@ -41,11 +49,14 @@ class SProducts(Base):
     product_name = Column(String)
     category_id = Column(Integer, ForeignKey('categories.category_id'))
 
+
 class SQuantity_products(Base):
     __tablename__ = 'quantity_products'
-    store = Column(ForeignKey('stores.store_id'))
+    storage_id = Column(Integer, primary_key=True, autoincrement=True)
+    storage_shop = Column(ForeignKey('shops.shop_id'))
     product_id = Column(Integer, ForeignKey('products.product_id'))
     quantity = Column(Integer)
+
 
 class SOrders(Base):
     __tablename__ = 'orders'
@@ -55,5 +66,3 @@ class SOrders(Base):
     product = Column(ForeignKey('products.product_id'))
     quantity = Column(Integer)
     price = Column(Integer)
-
-
